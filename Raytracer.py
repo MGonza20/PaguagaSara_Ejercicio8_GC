b9971b9d24c06b8f23072854dcb47f2fe1cc6a96
MGonza20 <sara.paguaga@gmail.com>
2022-09-30 17:48:54 -0600

    change

--- Raytracer.py
+++ Raytracer.py
@@ -4,12 +4,13 @@ from figures import *
 from lights import *
 
 
-width = 1024
-height = 1024
+width = 256
+height = 256
 
 # Materiales
 brick = Material(diffuse = (0.8, 0.3, 0.3), spec = 16)
 stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8)
+white = Material(diffuse = (0.1, 0.1, 0.1), spec = 8)
 
 glass = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 1.5, matType= TRANSPARENT)
 diamond = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 2.417, matType= TRANSPARENT)
@@ -24,7 +25,7 @@ rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
 
 rtx.scene.append(Plane(position = (0, -10, 0), normal = (0, 1, 0), material = brick))
 rtx.scene.append(Plane(position = (-20, 0, 0), normal = (1, 0, 0), material = stone))
-# rtx.scene.append(Plane(position = (15, 30, -15), normal = (-1, -1, -1), material = mirror))
+# rtx.scene.append(Plane(position = (-60, 0, 0), normal = (1, 0, 0), material = white))
 
 rtx.glRender()
 
