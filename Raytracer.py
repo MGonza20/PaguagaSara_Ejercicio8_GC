4618b23624f86bfa3d5f6df5a402c34a397293ea
MGonza20 <sara.paguaga@gmail.com>
2022-09-30 19:34:40 -0600

    displaying four cubes different cubes

--- Raytracer.py
+++ Raytracer.py
@@ -28,6 +28,10 @@ rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
 # rtx.scene.append(Plane(position = (-60, 0, 0), normal = (1, 0, 0), material = white))
 
 rtx.scene.append( AABB(position= (-2, -2, -10), size = (2,2,2), material = brick))
+rtx.scene.append( AABB(position= (2, -2, -10), size = (2,2,2), material = stone))
+
+rtx.scene.append( AABB(position= (-2, 2, -10), size = (2,2,2), material = mirror))
+rtx.scene.append( AABB(position= (2, 2, -10), size = (2,2,2), material = glass))
 
 
 rtx.glRender()
